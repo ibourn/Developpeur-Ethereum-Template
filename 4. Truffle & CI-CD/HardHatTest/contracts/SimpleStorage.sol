a65b0461b938b4461b774a1c9c3812e40bbd0792
ibourn <igor.bournazel@orange.fr>
2023-06-03 00:34:46 +0200

    hardhat test

---
+++ 4. Truffle & CI-CD/HardHatTest/contracts/SimpleStorage.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+//sepolia address 0x1E45a9511f83051be7Ab33D68cF92BD4B5278E20
+contract SimpleStorage {
+
+    uint private number;
+
+    constructor(uint _number) {
+        number = _number;
+    }
+
+    function setNumber(uint _number) external {
+        number = _number;
+    }
+
+    function getNumber() external view returns(uint) {
+        return number;
+    }
+
+}
---
+++ 4. Truffle & CI-CD/HardHatTest/scripts/deploy.js
@@ -0,0 +1,57 @@
+// // We require the Hardhat Runtime Environment explicitly here. This is optional
+// // but useful for running the script in a standalone fashion through `node <script>`.
+// //
+// // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// // will compile your contracts, add the Hardhat Runtime Environment's members to the
+// // global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+//   const Lock = await hre.ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+//   await lock.deployed();
+
+//   console.log(
+//     `Lock with ${ethers.utils.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+const hre = require("hardhat");
+
+async function main() {
+  const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy(777);
+
+  await simpleStorage.deployed();
+
+  console.log(`SimpleStorage deployed to ${simpleStorage.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+/*
+commande npm :
+npx hardhat compile
+2nd terminal (MEME DOSSIER!) : npx hardhat node
+npx hardhat run .\scripts\deploy.js --network localhost
+
+npx hardhat run ./scripts/deploy.js --network sepolia
+*/
